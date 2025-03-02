1eadb50dadfed5669a6c933647e9241f88a2570a
Jack Sabine <jack@sabine.ws>
2025-03-02 00:35:33 -0800

    Remove qsim and re-install as submodule

--- proj/project.sh
+++ proj/project.sh
@@ -4,6 +4,7 @@ export WORKAREA="${PWD}"
 export DV_ROOT="${WORKAREA}/dv"
 export RTL_ROOT="${WORKAREA}/rtl"
 export SCRIPTS_ROOT="${WORKAREA}/scripts"
+scripts=("${SCRIPTS_ROOT}/qsim")
 
 export DV_REGRESSION_LISTS_DIR="${DV_ROOT}/regressions"
 export DV_MODELS="${DV_ROOT}/model"
@@ -11,4 +12,6 @@ export DV_DPI_C="${DV_MODELS}/dpi-c"
 
 export WORKDIR="${WORKAREA}/work"
 export REGRDIR="${WORKAREA}/regr"
-export PATH="${SCRIPTS_ROOT}:${PATH}"
+for s in "${scripts[@]}"; do
+  export PATH="${s}:${PATH}"
+done
